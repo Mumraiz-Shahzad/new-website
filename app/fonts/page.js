7388245d5fcaf683e9a6f3b995db18af65036870
Mumraiz Shahzad <mumraiz1996@gmail.com>
2024-12-09 15:36:04 +0500

    app.js

---
+++ app/fonts/page.js
@@ -0,0 +1,10 @@
+// For the pages directory structure
+export default function Custom404() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>404 - Page Not Found</h1>
+        <p>The page you're looking for does not exist.</p>
+      </div>
+    );
+  }
+  
