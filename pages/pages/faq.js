e3713c213318e834e3436adea7c6cd773e2a3179
Mumraiz Shahzad <mumraiz1996@gmail.com>
2024-12-09 13:42:54 +0500

    FAQS Add

---
+++ pages/pages/faq.js
@@ -0,0 +1,13 @@
+// pages/pages/faq.js
+import React from 'react';
+
+const FAQ = () => {
+  return (
+    <div>
+      <h1>FAQ Page</h1>
+      {/* Your FAQ content here */}
+    </div>
+  );
+};
+
+export default FAQ;
